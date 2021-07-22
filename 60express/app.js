9651c551fe1bf747a00ad82ca9737f7b2119eb04
ankitbanerjee0211 <ankitbanerjee0211@gmail.com>
2021-07-22 17:43:43 +0530

    Update 24

--- 60express/app.js
+++ 60express/app.js
@@ -1,18 +1,25 @@
 // including express
 const express = require('express');
 const path = require('path');
+const fs = require('fs');
+
 const app = express();
 const port = 80;
 
-// EXPRESS SPECIFIC STUFF
-app.use('/static', express.static('static')); // For serving static files
+//// EXPRESS SPECIFIC STUFF
+app.use('/static', express.static('static')); // For serving static files.
+// using midware
+app.use(express.urlencoded());
+// forms data is colleted by express
 
-// PUG SPECIFIC STUFF/CONFIGURATION
+//// PUG SPECIFIC STUFF/CONFIGURATION
 app.set('view engine', 'pug'); // Set the template engine as pug
-app.set('views', path.join(__dirname, 'views')); // Set the views directory
+
+//// Set the views directory
+app.set('views', path.join(__dirname, 'views'));
 
 
-// ENDPOINTS
+//// ENDPOINTS
 app.get('/',(req, res)=>{
     // to send data to the pug file
     const con = "This is the basic template engine in express js.";
@@ -20,13 +27,28 @@ app.get('/',(req, res)=>{
     // to start rendering
     res.status(200).render('index.pug', params);
 });
+app.post('/', (req, res)=>{
+    name = req.body.name;
+    age = req.body.age;
+    gender = req.body.gender;
+    address = req.body.address;
+    more = req.body.more;
+    //these r the name attribute of the input tags NOT ID
 
-// pug help
+    // saving the data 
+    let outputToWrite = `The name of the client is ${name}, ${age} years old, ${gender}, residing at ${address}. More about him/her: ${more}.`
+    fs.writeFileSync('output.txt', outputToWrite) //path,element
+
+    const params = {'message': 'Your form has been submitted successfully'};
+    res.status(200).render('index.pug', params);
+});
+
+//// pug help
 // app.get("/demo", (req, res)=>{
 //     res.status(200).render('demo', { title:"Family Guy", message: 'Hello there!'});
 // });
 
-// START THE SERVER
+//// START THE SERVER
 app.listen(port, ()=>{
     console.log(`The application started successfully on ${port}`);
 });
---
+++ Dance_Website/app.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = 8000;
+
+//// Step-1 creating package.json
+/// 'npm init' in terminal
+
+/// Step-2 installing express and pug
+// 'npm i package_name' in terminal
+
+/// Step-3 creating static and views folder
+
+//// EXPRESS SPECIFIC STUFF
+/// Step-5 for serving static files(
+app.use('/static', express.static('static'));
+
+/// Step-6 to stop showing req data in the url
+app.use(express.urlencoded());
+
+//// PUG SPECIFIC STUFF
+/// Step-7 set the template engine as pug
+app.set('view engine', 'pug');
+/// Step-8 set the views directory
+app.set('views', path.join(__dirname, 'views')); //(setting, file path)
+
+//// Step-9 endpoints
+app.get('/', (req, res)=>{
+    const params = {}
+    res.status(200).render('index.pug', params);
+})
+
+
+//// Step-10 start the server
+// Listen to the port
+app.listen(port, ()=>{
+    console.log(`The application started successfully on port ${port}`);
+});
